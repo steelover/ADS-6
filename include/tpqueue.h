c0a126119f58ce33cb064570c40375b5e75cc84b
steelover <150594943+steelover@users.noreply.github.com>
2024-04-23 21:47:17 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,36 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
-};
+   private:
+    T data[100] = {};
+    int ind = 0, start = 0;
+
+   public:
+    T pop() {
+        start += 1;
+        return data[start - 1];
+    }
+
+    void push(T element) {
+        data[ind] = element;
+        ind += 1;
+        for (int t = start; t < ind; t++) {
+            for (int i = start; i < ind - 1; j++) {
+                if data
+                    [i + 1].prioritet > (data[i].prioritet) {
+                        T b = data[i];
+                        data[i] = data[i + 1];
+                        data[i + 1] = b;
+                    }
+            }
+        }
+    }
+}
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prioritet;
 };
-
-#endif  // INCLUDE_TPQUEUE_H_
+}
